98a149b58f7005a315059dd9aeefb9f54bf6b7ec
Bruno Melo <brunomelo1315@gmail.com>
2024-09-24 18:52:42 -0300

    fix imports and adjust jobs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from etl.raw.clubs_profile.job import ClubsProfileJob
 from request.ClubsPlayers import ClubsPlayers
 from request.ClubsProfile import ClubsProfile
 from request.CompetitionClubs import CompetitionClubs
@@ -6,11 +7,20 @@ from request.PlayersTransfers import PlayersTransfers
 
 
 def lambda_handler(event=None, context=None):
+    request()
+    etl()
+
+
+def request():
     CompetitionClubs().run()
     ClubsProfile().run()
     ClubsPlayers().run()
     PlayersMarketValue().run()
     PlayersTransfers().run()
+
+
+def etl():
+    ClubsProfileJob().main()
 
 
 if __name__ == "__main__":
