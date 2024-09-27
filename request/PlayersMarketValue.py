7a1edcebab0a4b4dda4644be38b63b43b53b2254
Bruno Melo <brunomelo1315@gmail.com>
2024-09-26 21:51:13 -0300

    :bug: fix clubs players read

--- request/PlayersMarketValue.py
+++ request/PlayersMarketValue.py
@@ -14,7 +14,10 @@ class PlayersMarketValue:
     }
 
     def get_player_ids(self) -> None:
-        club_players = read_json("./amostra/landing/ClubsPlayers/ClubsPlayers.json")
+        club_players = read_json(
+            bucket_name="tech-challenge-3-landing-zone",
+            path="ClubsPlayers/ClubsPlayers.json",
+        )
         players_lst: List[str] = list()
         for club in club_players:
             for player in club.get("players"):
