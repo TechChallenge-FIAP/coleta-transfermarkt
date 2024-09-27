b600b0c290c0689169d6a70fd0eaabb0b973ef6b
Bruno Melo <brunomelo1315@gmail.com>
2024-09-26 21:36:33 -0300

    :bug: fix request to support paginated APIs

--- models/players_injuries.py
+++ models/players_injuries.py
@@ -1,12 +1,25 @@
-from pydantic import BaseModel
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
 
 from models.util import SoccerInfo
+
+
+class Injury(BaseModel):
+    season: Optional[str] = None
+    injury: Optional[str] = None
+    injuryFrom: Optional[str] = Field(None, alias="from")
+    until: Optional[str] = None
+    days: Optional[str] = None
+    gamesMissed: Optional[str] = None
+    gamesMissedClubs: Optional[List[str]] = None
 
 
 class PlayersInjuries(BaseModel):
     id: str
     pageNumber: int
     lastPageNumber: int
+    injuries: Optional[List[Injury]] = None
     updatedAt: str
 
 
